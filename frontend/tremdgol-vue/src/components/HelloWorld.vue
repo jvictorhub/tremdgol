f3c9aa79b5082ef3d81d203efe62c94e79756ea0
jvictorhub <jvwr.cin@gmail.com>
2022-09-04 22:53:59 -0300

    frontend initial release 0.1.0 vue+vuetify

---
+++ frontend/tremdgol-vue/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    Tremdgol
+    <h1>Welcome!</h1>
+    <h3>Choose one event of the list above to see more details and live stats.</h3>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: () => ({}),
+};
+</script>
